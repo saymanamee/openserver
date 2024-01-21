39958ede238507a00dde363d520137a4c30857b1
Nikita <mike7331wp@gmail.com>
2024-01-21 17:53:24 +0300

    1

---
+++ openserver/openserver.cpp
@@ -0,0 +1,47 @@
+﻿// Подключаем необходимые библиотеки для работы с базой данных
+#include <iostream>
+#include "mysql_connection.h"
+#include "cppconn/driver.h"
+#include "cppconn/exception.h"
+#include "cppconn/resultset.h"
+#include "cppconn/statement.h"
+using namespace std;
+
+int main(void)
+{
+    cout << endl;
+    cout << "Running 'SELECT 'Hello World!' >> AS _message'..." << endl;
+        try {
+        sql::Driver* driver;
+        sql::Connection* con;
+        sql::Statement* stmt;
+        sql::ResultSet* res;
+
+        /* Create a connection */
+        driver = get_driver_instance();
+        con = driver->connect("tcp://127.0.0.1:3306", "root", "admin");
+        /* Connect to the MySQL test database */
+        con->setSchema("mydatabase");
+        stmt = con->createStatement();
+        res = stmt->executeQuery("CREATE TABLE IF NOT EXISTS table1 (id integer, name varchar(255), surname varchar(255))");
+        while (res->next()) {
+            cout << "\t... MySQL replies: ";
+            /* Access column data by alias or column name */
+            cout << res->getString("_message") << endl;
+            cout << "\t... MySQL says it again: ";
+            /* Access column data by numeric offset, 1 is the first column */
+            cout << res->getString(1) << endl;
+        }
+        delete res;
+        delete stmt;
+        delete con;
+    }
+    catch (sql::SQLException& e) {
+        cout << "# ERR: SQLException in " << __FILE__;
+        cout << "# ERR: " << e.what();
+        cout << " (MySQL error code: " << e.getErrorCode();
+        cout << ", SQLState: " << e.getSQLState() << " )" << endl;
+    }
+    cout << endl;
+    return EXIT_SUCCESS;
+}
